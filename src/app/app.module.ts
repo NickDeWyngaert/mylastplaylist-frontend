a7c1e76ab780b9e237e1133497420d2f5028140e
Nick De Wyngaert <43341508+NickDeWyngaert@users.noreply.github.com>
2021-10-18 15:55:45 +0200

    Removed old playlists component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PlaylistsComponent } from './playlists/playlists.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlaylistsComponent,
-    HomeComponent,
-    PlaylistComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, PlaylistComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
