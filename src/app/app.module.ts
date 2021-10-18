2df5bbe961590cb690816cfb81213e2dcd80b739
Nick De Wyngaert <43341508+NickDeWyngaert@users.noreply.github.com>
2021-10-18 16:42:24 +0200

    Add new song to playlist of user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,8 @@ import { PlaylistComponent } from './playlist/playlist.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -3,6 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Playlist } from '../playlist';
 import { PlaylistService } from '../playlist.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Song } from '../song';
 
 @Component({
   selector: 'app-playlist',
@@ -12,20 +19,39 @@ import { PlaylistService } from '../playlist.service';
 export class PlaylistComponent implements OnInit {
   done: boolean = false;
   playlist: Playlist | null = null;
+  addform: FormGroup = this.fb.group({});
+  private urlRegex =
+    /^[A-Za-z][A-Za-z\d.+-]*:\/*(?:\w+(?::\w+)?@)?[^\s/]+(?::\d+)?(?:\/[\w#!:.?+=&%@\-/]*)?$/;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private service: PlaylistService
+    private service: PlaylistService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: Params) => {
       let userid: number = <number>params['id'];
       this.getPlaylistWithUserId(userid);
+      this.addform = this.fb.group({
+        title: new FormControl('', [
+          Validators.required,
+          Validators.nullValidator,
+        ]),
+        artist: new FormControl('', [
+          Validators.required,
+          Validators.nullValidator,
+        ]),
+        link: new FormControl('', [
+          Validators.required,
+          Validators.nullValidator,
+          //Validators.pattern(this.urlRegex), // doesn't work anymore?
+        ]),
+      });
     });
   }
 
-  private getPlaylistWithUserId(id: number) {
+  private getPlaylistWithUserId(id: number): void {
     this.service.get(id).subscribe(
       (playlist: Playlist) => {
         this.playlist = playlist;
@@ -35,5 +61,33 @@ export class PlaylistComponent implements OnInit {
         console.log('Error while fetching playlist with id', error);
       }
     );
+  }
+
+  get title() {
+    return this.addform.get('title');
+  }
+  get artist() {
+    return this.addform.get('artist');
+  }
+  get link() {
+    return this.addform.get('link');
+  }
+
+  newSong(): void {
+    let newSong: Song = <Song>this.addform.value;
+    let userId: number = this.playlist?.user.id!;
+    this.service.post(userId, newSong).subscribe(
+      (playlist: Playlist) => {
+        this.resetAddform();
+        this.getPlaylistWithUserId(userId);
+      },
+      (error: HttpErrorResponse) => {
+        console.log('Failed to add new song', error);
+      }
+    );
+  }
+
+  resetAddform(): void {
+    this.addform.reset();
   }
 }
